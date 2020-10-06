42615ae86114e1179fc6b611099ac4fb528ebfd7
Mokytojas <Mokytojas@KITM.local>
2020-10-06 11:22:18 +0300

    begin

---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+
+
+export default function Main() {
+    return (
+       
+           <div>
+               <p>Main sritis</p>
+            </div>
+    );
+};
+
